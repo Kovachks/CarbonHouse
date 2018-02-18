146bfd76652ae444c98de477b1bdefaeca8d78c5
Keith Kovach <kovachks90@gmail.com>
2018-02-17 22:20:18 -0500

    updated css for 1024 width tablets

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,13 @@
 function overlayGenerate() {
-    var concerts = [".tourDay.0", ".tourDay.1", ".tourDay.2", ".tourDay.3", ".tourDay.4", ".tourDay.5",]    
     for (var i = 0; i < 6; i += 1) {
-        var element = concerts[i]
+    
+        var element = ".tourDay." + i
+    
+        console.log(element)
+    
         var width = ($(element).width())
-        $(element).children().css('right', (width + 11) + 'px')
+    
+        $(element).children().css('right', (width + 10.5) + 'px')
     }    
 }
 
