677ee7a94d4899912966bf4b1e2e4a51ef22ac31
Keith Kovach <kovachks90@gmail.com>
2018-02-26 14:17:38 -0500

    Main Project Complete!  Beginning on cleanup

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,6 +9,7 @@ function overlayGenerate() {
     }    
 }
 
+
 function loginPadding() {
     var windowWidth = $(window).width()
     $(".login").css("padding-right", windowWidth * .057)
@@ -20,6 +21,35 @@ $(document).ready(function() {
 })
 
 window.onresize = function() {
+    var windowWidth = $(window).width()
+    console.log(windowWidth)
+    if (windowWidth < 991) {
+        $(".grid").css("background-image", "url('./assets/images/grid_checked.png'")
+        $(".list").css("background-image", "url('./assets/images/hamburger_unchecked.png")
+        $(".artistNameTour").removeAttr("style")
+        $(".desktopArtistInfo").removeAttr("style")
+        $(".artistHolder").switchClass("col-md-12", "col-md-4")
+        $(".artistImage").removeAttr("style")
+    }
     loginPadding()
 }
-// overlayGenerate()
+
+$(".list").on("click", function() {
+    $(".grid").css("background-image", "url('./assets/images/grid_unchecked.png'")
+    $(".list").css("background-image", "url('./assets/images/hamburger_checked.png")
+    $(".artistNameTour").css("display", "block")
+    $(".desktopArtistInfo").css("display", "none")
+    $(".artistHolder").switchClass("col-md-4", "col-md-12")
+    $(".artistImage").css("width", "20%")
+    $(".artistNameTour").css({"width": '80%', "padding": "41px 0",})
+    $("").css("display", "block")
+})
+
+$(".grid").on("click", function() {
+    $(".grid").css("background-image", "url('./assets/images/grid_checked.png'")
+    $(".list").css("background-image", "url('./assets/images/hamburger_unchecked.png")
+    $(".artistNameTour").css("display", "none")
+    $(".desktopArtistInfo").css("display", "inline-block")
+    $(".artistHolder").switchClass("col-md-12", "col-md-4")
+    $(".artistImage").css("width", "100%")
+})
