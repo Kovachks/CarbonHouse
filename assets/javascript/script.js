d68f4c28eb9deb8d52761fd3c98c4eed6e5252a4
Keith Kovach <kovachks90@gmail.com>
2018-02-27 10:43:25 -0500

    firefox updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,32 +9,22 @@ function overlayGenerate() {
     }    
 }
 
-
 function loginPadding() {
     var windowWidth = $(window).width()
     $(".login").css("padding-right", windowWidth * .057)
 }
 
-$(document).ready(function() {
-    loginPadding()
-    overlayGenerate()
-})
-
-window.onresize = function() {
-    var windowWidth = $(window).width()
-    console.log(windowWidth)
-    if (windowWidth < 991) {
-        $(".grid").css("background-image", "url('./assets/images/grid_checked.png'")
-        $(".list").css("background-image", "url('./assets/images/hamburger_unchecked.png")
-        $(".artistNameTour").removeAttr("style")
-        $(".desktopArtistInfo").removeAttr("style")
-        $(".artistHolder").switchClass("col-md-12", "col-md-4")
-        $(".artistImage").removeAttr("style")
-    }
-    loginPadding()
+function gridLayout() {
+    $(".grid").css("background-image", "url('./assets/images/grid_checked.png'")
+    $(".list").css("background-image", "url('./assets/images/hamburger_unchecked.png")
+    $(".artistNameTour").removeAttr("style")
+    $(".desktopArtistInfo").removeAttr("style")
+    $(".artistHolder").switchClass("col-md-12", "col-md-4")
+    $(".artistImage").removeAttr("style")
+    $(".buyTicketsLink").removeAttr("style")
 }
 
-$(".list").on("click", function() {
+function listLayout() {
     $(".grid").css("background-image", "url('./assets/images/grid_unchecked.png'")
     $(".list").css("background-image", "url('./assets/images/hamburger_checked.png")
     $(".artistNameTour").css("display", "block")
@@ -43,13 +33,26 @@ $(".list").on("click", function() {
     $(".artistImage").css("width", "20%")
     $(".artistNameTour").css({"width": '80%', "padding": "41px 0",})
     $("").css("display", "block")
+    $(".buyTicketsLink").css("display", "inline-block")
+}
+
+$(document).ready(function() {
+    loginPadding()
+    overlayGenerate()
+    $(".list").on("click", function() {
+        listLayout()
+    })
+    
+    $(".grid").on("click", function() {
+        gridLayout()
+    })
 })
 
-$(".grid").on("click", function() {
-    $(".grid").css("background-image", "url('./assets/images/grid_checked.png'")
-    $(".list").css("background-image", "url('./assets/images/hamburger_unchecked.png")
-    $(".artistNameTour").css("display", "none")
-    $(".desktopArtistInfo").css("display", "inline-block")
-    $(".artistHolder").switchClass("col-md-12", "col-md-4")
-    $(".artistImage").css("width", "100%")
-})
+window.onresize = function() {
+    var windowWidth = $(window).width()
+    if (windowWidth < 991) {
+        gridLayout()
+    }
+    loginPadding()
+}
+
