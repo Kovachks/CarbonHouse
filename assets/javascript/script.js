46e856b33adfc3f9cba95504c92e044dbb502247
Keith Kovach <kovachks90@gmail.com>
2018-03-06 13:37:31 -0500

    squashed some css bugs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+//Generates dynamic "triangles" for each of the performance days on each artist
 function overlayGenerate() {
     for (var i = 0; i < 6; i += 1) {
     
@@ -9,24 +10,13 @@ function overlayGenerate() {
     }    
 }
 
-$(document).ready(function() {
-    loginPadding()
-    overlayGenerate()
-    $(".list").on("click", function() {
-        console.log("test")
-        listLayout()
-    })
-    
-    $(".grid").on("click", function() {
-        gridLayout()
-    })
-})  
-
+//Function to generate padding for login button based on current window width
 function loginPadding() {
     var windowWidth = $(window).width()
     $(".login").css("padding-right", windowWidth * .057)
 }
 
+//Switch from list layout to grid layout at desktop view
 function gridLayout() {
     document.getElementById("grid").style.backgroundImage = "url('./assets/images/grid_checked.png')"
     document.getElementById("list").style.backgroundImage = "url('./assets/images/hamburger_unchecked.png')"
@@ -37,6 +27,7 @@ function gridLayout() {
     $(".buyTicketsLink").removeAttr("style")
 }
 
+//Switch from grid layout to list layout at desktop view
 function listLayout() {
     document.getElementById("grid").style.backgroundImage = "url('./assets/images/grid_unchecked.png')"
     document.getElementById("list").style.backgroundImage = "url('./assets/images/hamburger_checked.png')"
@@ -49,24 +40,66 @@ function listLayout() {
     $(".buyTicketsLink").css("display", "inline-block")
 }
 
+//Function to dynamically resize header based on width of window
+function tabletHeaderSize() {
+    var windowWidth = $(window).width()
+    if (windowWidth > 752 && windowWidth < 975) {
+        $("#header").removeAttr("style")
+        document.getElementById("header").style.width = ((windowWidth*.886) - 30) + 'px'
+        document.getElementById("header").style.marginLeft = ((windowWidth*.045) + 26) + 'px'
+        document.getElementById("header").style.marginRight = ((windowWidth*.045) + 15) + 'px' 
+    }
+    else if (windowWidth > 974) {
+        $("#header").removeAttr("style")
+        document.getElementById("header").style.width = ((windowWidth*.91) - 30) + 'px'
+        document.getElementById("header").style.marginLeft = ((windowWidth*.045) + 15) + 'px'
+        document.getElementById("header").style.marginRight = ((windowWidth*.045) + 15) + 'px' 
+    }
+    else {
+        $("#header").removeAttr("style")
+    }
+}
+
 $(document).ready(function() {
     loginPadding()
     overlayGenerate()
+    tabletHeaderSize()
+
+    //Click handler for list layout
     $(".list").on("click", function() {
         console.log("test")
         listLayout()
     })
     
+    //Click handler for grid layout
     $(".grid").on("click", function() {
         gridLayout()
     })
 })
 
+//Function to automatically revert to grid layout once a certain window width is achieved
 window.onresize = function() {
     var windowWidth = $(window).width()
     if (windowWidth < 991) {
         gridLayout()
     }
+
+    tabletHeaderSize()
     loginPadding()
 }
 
+$(document).ready(function() {
+    loginPadding()
+    overlayGenerate()
+
+    //Click handler for list layout
+    $(".list").on("click", function() {
+        console.log("test")
+        listLayout()
+    })
+    
+    //Click handler for grid layout
+    $(".grid").on("click", function() {
+        gridLayout()
+    })
+})
